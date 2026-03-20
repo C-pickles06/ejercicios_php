d73177fd6a73daa6dc48a34c45f27ed7af479a7f
Cesar Herrera <cesarheal06@gmail.com>
2026-03-20 17:53:37 -0500

    feat:Agrega ejercicios de php

---
+++ ejercicio_listas.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Quieres ver los ejercicios: </p>
+    <br>
+    <ol>
+    <li>
+        <a href="listas_de_tareas.php">lista de tareas</a>
+    </li>
+    <li>
+        <a href="carrito_de_compras.php">Carrito de compras</a>
+    </li>
+    </ol>
+    <h2>Teoria del instructor</h2>
+    <ul>
+        <li><a href="C:\Users\Aprendiz\Desktop\material\material">hola</a></li>
+    </ul>
+</body>
+</html>
---
+++ listas_de_tareas.php
@@ -0,0 +1,21 @@
+<?php 
+session_start();
+$_SESSION['tarea']=[];
+if (isset($_POST['tarea'])) {
+    $a = $_POST['tarea'];
+    $_SESSION['tarea'][]= $a;
+
+}
+?>
+<form method="post">
+    <p>Ingresa tu tarea</p>
+    <input type="text" name="tarea">
+    <input type="submit">
+</form>
+<ul>
+<?php 
+if(isset($_SESSION['tarea'])){
+
+
+?>
+<?= "<li>{$a}</li>"?>}</ul>
