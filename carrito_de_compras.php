272e692f574fc41fe022936433cd9d2d15a2f956
Cesar Herrera <cesarheal06@gmail.com>
2026-03-25 15:49:55 -0500

    feat: Se agregó logica para encontrar el id de un producto, falta que haga el calculo de los productos.

--- carrito_de_compras.php
+++ carrito_de_compras.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 $producto_1 = "Mouse";
 $producto_2 = "Teclado";
@@ -7,62 +7,75 @@ $producto_4 = "Cargador";
 
 #Array asociativo para albergar todos los productos con su precio
 
-$hola = [["id"=> 1,"nombre" => $producto_1,"precio"=> 1000],
-["id"=> 2,"nombre"=> $producto_2, "precio" =>2000],
-["id"=> 3,"nombre" => $producto_3, "precio" => 3000]];
+$hola = [
+    ["id" => 1, "nombre" => $producto_1, "precio" => 1000],
+    ["id" => 2, "nombre" => $producto_2, "precio" => 2000],
+    ["id" => 3, "nombre" => $producto_3, "precio" => 3000],
+    ["id" => 4, "nombre" => $producto_4, "precio" => 4000]
+];
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
+    <!-- Todos los objetos para agregar al carrito -->
     <h1>Carrito de compras</h1>
-<form method="post">
-<div>
-    <p>Mouse<br>$<?= "{$producto_1}" ?></p>
-    <input type="number" name="cantidad" type="number" min="1">
-    <input type="hidden" value="1" name="subir">
-    <input type="submit">
-</div>
-<!-- Todos los parrafos-->
- <div>
-    <p>Teclado <br>$<?= "{$producto_2}" ?></p>
-    <input type="number" name="cantidad" type="number" min="1">
-    <input type="hidden" value="2" name="subir">
-    <input type="submit">
-</div>
- <div>
-    <p>Audifonos <br> $<?= "{$producto_3}" ?></p>
-    <input type="number" name="cantidad" type="number" min="1">
-    <input type="hidden" value="3" name="subir">
-    <input type="submit">
-</div>
- <div>
-    <p>Cargador <br> $<?= "{$producto_4}" ?></p>
-    <input type="number" name="cantidad" type="number" min="1">
-    <input type="hidden" value="4" name="subir">
-    <input type="submit">
- </div>
-<br><br><button type="submit" name="calcular">Calcular</button>
-</form>
+    <form method="post">
+        <div>
+            <p>Mouse<br>$<?= "{$producto_1}" ?></p>
+            <input type="number" name="cantidad" type="number" min="1" value="1">
+            <button type="submit" name="subir" value="1"> Enviar</button>
+        </div>
+
+        <div>
+            <p>Teclado <br>$<?= "{$producto_2}" ?></p>
+            <input type="number" name="cantidad" type="number" min="1" value="1">
+            <button type="submit" name="subir" value="2"> Enviar</button>
+        </div>
+        <div>
+            <p>Audifonos <br> $<?= "{$producto_3}" ?></p>
+            <input type="number" name="cantidad" type="number" min="1" value="1">
+            <button type="submit" name="subir" value="3"> Enviar</button>
+        </div>
+        <div>
+            <p>Cargador <br> $<?= "{$producto_4}" ?></p>
+            <input type="number" name="cantidad" type="number" min="1" value="1">
+            <button type="submit" name="subir" value="4"> Enviar</button>
+        </div>
+        <br><br><button type="submit" name="calcular">Calcular</button>
+    </form>
 </body>
+
 </html>
 
 
-<?php 
-$idBuscado = isset($_POST['subir']) ? (int)$_POST['id'] : 1;
-$resultado = array_filter($hola, function ($item) use ($idBuscado){
-    return $item["id"] == $idBuscado;
+<?php
+$id_buscado = $_POST['subir'];
+echo $id_buscado;
+if (isset($_POST['subir'])) {
+    $resultado = array_filter($hola, function ($item) use ($id_buscado) {
+    return $item["id"] == $id_buscado;
 });
-$nombre_prod= reset($resultado);
-$_SESSION['carrito']=[];
-if($nombre_prod){
-    echo "producto: ".$nombre_prod["nombre"]. " - $".$nombre_prod["precio"];
+$nombre_prod = reset($resultado);
+    $_SESSION['carrito']=[];
 }
+
+if ($nombre_prod) {
+    echo "producto: " . $nombre_prod["nombre"] . " - $" . $nombre_prod["precio"];
+}
+
+
 /*
+
+
+
+
 $iva= 0.19;
 $cantidad= intval($_POST['cantidad']);
 
@@ -70,7 +83,7 @@ foreach ($hola as $i) {
 
     print_r($i);
 }
-#$calcular = $producto_1 * $iva;
-#echo $calcular;
+$calcular = $producto_1 * $iva;
+echo $calcular;
 */
 ?>
