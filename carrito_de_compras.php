d7c5dcbeaddd760bbc392bd6ee17face3193e4db
C-pickles06 <cesarheal06@gmail.com>
2026-03-26 11:57:49 -0500

    feat: Ejercicio del carrito de compras terminado

--- carrito_de_compras.php
+++ carrito_de_compras.php
@@ -13,6 +13,37 @@ $hola = [
     ["id" => 3, "nombre" => $producto_3, "precio" => 3000],
     ["id" => 4, "nombre" => $producto_4, "precio" => 4000]
 ];
+
+if (isset($_POST['subir'])) {
+    $id_buscado = $_POST['subir'];
+    $cantidad = intval($_POST['cantidad']);
+
+    $resultado = array_filter($hola, function ($item) use ($id_buscado) {
+        return $item['id'] == $id_buscado;
+    });
+    $producto_e = reset($resultado);
+
+    if ($producto_e) {
+        if (isset($_SESSION['Carrito'][$id_buscado])) {
+            $_SESSION['carrito'][$id_buscado]['cantidad'] += $cantidad;
+        } else {
+            $_SESSION['carrito'][$id_buscado] = [
+                'id' => $producto_e['id'],
+                'nombre' => $producto_e['nombre'],
+                'precio' => $producto_e['precio'],
+                'cantidad' => $cantidad,
+            ];
+        }
+    }
+}
+echo "Producto agregado: " . $producto_e["nombre"] . " - $" . $producto_e["precio"] . " x" . $cantidad . "<br>";
+
+if(!empty($_SESSION['carrito'])){
+    echo "<h2>Carrito actual</h2>";
+    foreach($_SESSION['carrito'] as $item){
+        echo $item['nombre']. "- $". $item['precio']. " X". $item['cantidad']. " = $".$item['cantidad'] . "<br>";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -27,63 +58,15 @@ $hola = [
     <!-- Todos los objetos para agregar al carrito -->
     <h1>Carrito de compras</h1>
     <form method="post">
-        <div>
-            <p>Mouse<br>$<?= "{$producto_1}" ?></p>
-            <input type="number" name="cantidad" type="number" min="1" value="1">
-            <button type="submit" name="subir" value="1"> Enviar</button>
-        </div>
-
-        <div>
-            <p>Teclado <br>$<?= "{$producto_2}" ?></p>
-            <input type="number" name="cantidad" type="number" min="1" value="1">
-            <button type="submit" name="subir" value="2"> Enviar</button>
-        </div>
-        <div>
-            <p>Audifonos <br> $<?= "{$producto_3}" ?></p>
-            <input type="number" name="cantidad" type="number" min="1" value="1">
-            <button type="submit" name="subir" value="3"> Enviar</button>
-        </div>
-        <div>
-            <p>Cargador <br> $<?= "{$producto_4}" ?></p>
-            <input type="number" name="cantidad" type="number" min="1" value="1">
-            <button type="submit" name="subir" value="4"> Enviar</button>
-        </div>
+        <?php foreach ($hola as $producto): ?>
+            <div>
+                <p><?= $producto["nombre"] ?><br>$<?= $producto["precio"] ?></p>
+                <input type="number" name="cantidad" type="number" min="1" value="1">
+                <button type="submit" name="subir" value="<?= $producto["id"] ?>"> Enviar</button>
+            </div>
+        <?php endforeach; ?>
         <br><br><button type="submit" name="calcular">Calcular</button>
     </form>
 </body>
 
 </html>
-
-
-<?php
-$id_buscado = $_POST['subir'];
-echo $id_buscado;
-if (isset($_POST['subir'])) {
-    $resultado = array_filter($hola, function ($item) use ($id_buscado) {
-    return $item["id"] == $id_buscado;
-});
-$nombre_prod = reset($resultado);
-    $_SESSION['carrito']=[];
-}
-
-if ($nombre_prod) {
-    echo "producto: " . $nombre_prod["nombre"] . " - $" . $nombre_prod["precio"];
-}
-
-
-/*
-
-
-
-
-$iva= 0.19;
-$cantidad= intval($_POST['cantidad']);
-
-foreach ($hola as $i) {
-
-    print_r($i);
-}
-$calcular = $producto_1 * $iva;
-echo $calcular;
-*/
-?>
